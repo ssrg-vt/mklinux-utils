3d8222d935ead7487915cce72072c4be7ff1d0fb
Antonio Barbalace <antoniob@vt.edu>
2013-07-31 16:09:45 -0400

    initial mklinux_boot.sh rewriting (with error checks)

--- mklinux_boot.sh
+++ mklinux_boot.sh
@@ -1,45 +1,54 @@
 #!/bin/bash
 
-if [[ "$USER" != "root" ]] ; then
-	echo "Only root can boot secondary kernels."
-	exit 1
-fi
-
 KERNEL=$1
 BOOT_ARGS=$2
 CPU=$3
 BOOT_ADDR=$4
 RAMDISK_ADDR=$5
+RAMDISK_FILE="ramdisk.img"
+#RAMDISK=$6
 
-echo "MKLINUX: loading ramdisk AT $RAMDISK_ADDR"
-./copy_ramdisk $RAMDISK_ADDR
-if [ $? ]
-do
-check for errors
+if [[ "$USER" != "root" ]] ; then
+	echo "Only root can boot secondary kernels."
+	exit 1
+fi
+
+if [[ $RAMDISK_FILE $RAMDISK_ADDR ]]
+then
   exit 1
-done
+fi 
+echo "MKLINUX: try loading $RAMDISK_FILE at address $RAMDISK_ADDR"
+$RESULT=`./copy_ramdisk $RAMDISK_ADDR $RAMDISK_FILE` 
+if [ $? ]
+then
+  exit 1
+fi
 
-echo "MKLINUX: setting boot arguments FROM $BOOT_ARGS"
+if [[ $BOOT_ARGS ]]
+then
+  exit 1
+fi
+echo "MKLINUX: setting boot arguments to $BOOT_ARGS"
 ./wr_cmdline $BOOT_ARGS
 if [ $? ]
-do
-check for errors
+then
   exit 1
-done
-
-
-
-
-
-
-echo "MKLINUX: loading kernel AT $BOOT_ADDR"
+fi
+./rd_cmdline
+ 
+if [[ $KERNEL $BOOT_ADDR ]]
+then
+  exit 1
+fi
+echo "MKLINUX: loading $KERNEL at address $BOOT_ADDR"
 kexec -d -a $BOOT_ADDR -l $KERNEL -t elf-x86_64 --args-none
 if [ $? ]
-do
-doe skexec returns errors?!
-check for errors
+then
+#check for eventual error codes in the return
   exit 1
-done
+fi
+
+exit 0
 
 echo "MKLINUX: booting kernel ON CPU $CPU"
 kexec -d -a $BOOT_ADDR -b $CPU
