c5685d25846af866274b62d7994e5b1ae54f926b
Antonio Barbalace <antoniob@vt.edu>
2013-10-21 22:07:14 -0400

    working on the library for the memory partitioning

--- lib/memory.h
+++ lib/memory.h
@@ -1,9 +1,16 @@
 
-int system_ram( /*return a list */ );
+typedef struct _memres {
+  long long start, end;
+} memres;
+
+#define BASE_MEMRES 16
 
 long long numa_node_size64(int node, long long *freep);
 
 int numa_node_cpumask(int node, cpu_bitmask_t * cpus);
+
+
+
 
 int numa_configured_cpus(void);
 
