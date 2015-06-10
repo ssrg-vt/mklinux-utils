1a640b2c9081aaf3e7889f497da4b545818d3d4f
marina <marina@minion.(none)>
2015-06-10 15:54:49 -0400

    popcorn.sh changed to take as parameter replication degree
    Former-commit-id: 0e990a02369d57b18adf2bf1a2996d56136ef543

--- ns/popcorn.sh
+++ ns/popcorn.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
 
-echo "activating poopcorn ns!"
-echo 0 > /proc/popcorn_namespace
-exec bash
+if [ -z "$1" ]
+then
+	echo "specify replication degree"
+
+elif [ $1 -lt 2 ] || [ $1 -gt 64 ]
+then
+	echo "replication degree $1 is invalid"
+else
+	echo "switching to poopcorn ns! Replication degree requested is $1"
+	#echo $1 > /proc/popcorn_namespace
+	#exec bash
+fi
 
