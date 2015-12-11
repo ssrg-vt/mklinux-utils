7dcade3cbd6fd30765aa8fbb11429f331fc55c3b
giuliano <giuliano@debian7-vm>
2015-12-11 14:58:42 -0500

    Fix in replication_tests.sh
    Former-commit-id: 1a5f56be5358b20d9642786590ac5db59bad217a

--- custom_ramdisk/packages/replication_tests/replication_tests.sh
+++ custom_ramdisk/packages/replication_tests/replication_tests.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 set -eu -o pipefail
 
+
+# set up the server test in mklinux-utils/benchmarks/TCPEcho/
+src_dir="${mklinux_utils}/benchmarks/TCPEcho/"
+server_c="TCPEchoServerFile.c"
+command -v gcc >/dev/null 2>&1 || fail "gcc not found"
+( cd ${src_dir}; gcc -static -o server "./${server_c}" ) || fail "failed to compile ${server_c}"
+
 target_dir="${image_root}/${mklinux_utils}/ns/"
 [ "${verbose}" = "y" ] && echo "target_dir is ${target_dir}"
 mkdir -p "$target_dir"
-( . "${scripts_dir}/copy_exec.sh" "${scripts_dir}/../benchmarks/TCPEcho/server" "$target_dir/" "server")
+( . "${scripts_dir}/copy_exec.sh" "${src_dir}/server" "$target_dir/" "server")
