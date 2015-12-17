aa10eda8831ec59e7cf5a84ddbac007f1eb64623
Giuliano Losa <glosa@vt.edu>
2015-12-17 13:31:03 -0500

    same slapd path in the image and in the primary
    Former-commit-id: 2955204208b9a499758b3480a586126d3c37ecaf

--- custom_ramdisk/packages/slapd/slapd.sh
+++ custom_ramdisk/packages/slapd/slapd.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 set -eu -o pipefail
 
+# downloads, compiles slapd, and copies it to the image.
+
 version="2.4.43"
 url="ftp://ftp.openldap.org/pub/OpenLDAP/openldap-release/openldap-${version}.tgz"
 file="./openldap-${version}.tgz"
-dir="./openldap-${version}"
+dir=$(absolute_path "./openldap-${version}")
 # fetch
 if [ ! -f "$file" ]; then
     wget "$url"
@@ -15,9 +17,9 @@ tar -xzf "$file"
 ( cd "$dir"; ./configure "--enable-debug"; make; make depend )
 # copy to image
 server="$dir/servers/slapd/slapd"
-( . "${scripts_dir}/copy_exec.sh" "$server" "$image_root" "/root/slapd")
+( . "${scripts_dir}/copy_exec.sh" "$server" "$image_root")
 config="$dir/servers/slapd/slapd.conf"
-cp "$config" "${image_root}/root/slapd.conf"
+cp "$config" "${image_root}/"$(dirname "$server")"/slapd.conf"
 
 # remove the dir where we unpacked?
 # rm -r "$dir"
