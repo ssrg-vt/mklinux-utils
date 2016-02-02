d8750a7c01a67f0b16b450f744e6169fb5a3c1a1
Yuzhong Wen <wyz2014@vt.edu>
2016-02-01 20:50:35 -0500

    Add mongoose
    Former-commit-id: 937c569dd7682137caa5b70295a7f0bf196b317d

--- custom_ramdisk/packages/mongoose/mongoose.sh
+++ custom_ramdisk/packages/mongoose/mongoose.sh
@@ -1,13 +1,8 @@
 #! /bin/sh
 #
 
-#VER=3.6
-#if [ ! -f mongoose-$VER.tgz ]; then
-#    wget http://mongoose.googlecode.com/files/mongoose-$VER.tgz
-#fi
-#tar zxvf mongoose-$VER.tgz
-#cd mongoose
-#make linux
-#mv mongoose ../mg-server
-#cd ..
+cd mongoose
+make linux
+mv mongoose ../mg-server
+cd ..
 ( . "${scripts_dir}/copy_exec.sh" "mg-server" "$image_root")
