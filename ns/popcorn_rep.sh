aa58e43061a1d2512cbb153874a097eb3512bb09
Yuzhong Wen <wyz2014@vt.edu>
2016-04-27 11:33:02 -0400

    Add a new popcorn ns script to load LD_PRELOAD lib
    Former-commit-id: 1ab59c960924bae7aecf1c1a603fbfaf484116fa

---
+++ ns/popcorn_rep.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+if [ -z "$1" ]
+then
+	echo "specify replication degree"
+
+elif [ $1 -lt 2 ] || [ $1 -gt 64 ]
+then
+	echo "replication degree $1 is invalid"
+else
+	echo "switching to poopcorn ns! Replication degree requested is $1"
+	echo $1 > /proc/popcorn_namespace
+	export LD_PRELOAD=/usr/lib/pcn_det.so
+	exec /bin/busybox sh
+fi
+
