80789dd6058ccd32b7ba84d3cd030a72dd44fcd3
Yuzhong Wen <wyz2014@vt.edu>
2016-05-09 23:32:11 -0400

    Update ld_preload library
    Former-commit-id: 0c0b9da542e1aab68ff116ca972326f5b46b3c1b

--- custom_ramdisk/packages/ld_preload/pcn_det.c
+++ custom_ramdisk/packages/ld_preload/pcn_det.c
@@ -14,6 +14,15 @@
 #include <dlfcn.h>
 
 #define PTHREAD_PATH "/lib/x86_64-linux-gnu/libpthread.so.0"
+#define MTX_LOCK
+#define MTX_UNLOCK
+//#define DEBUG
+#define SCHED_REP
+#ifdef DEBUG
+#define PRINT(...) printf(__VA_ARGS__)
+#else
+#define PRINT(...);
+#endif
 
 typedef int8_t atomic8_t;
 typedef uint8_t uatomic8_t;
@@ -70,6 +79,8 @@ typedef uintmax_t uatomic_max_t;
 #define LLL_PRIVATE 128
 #define LLL_SHARED 0
 
+#define ETIMEDOUT 60
+
 #define atomic_compare_and_exchange_bool_acq(mem, newval, oldval) \
       (! __sync_bool_compare_and_swap (mem, oldval, newval))
 
@@ -99,6 +110,9 @@ typedef uintmax_t uatomic_max_t;
 
 #define lll_futex_wait(futex, val, private)     \
         syscall(202, futex, FUTEX_WAIT_PRIVATE, val, NULL, NULL, 0)
+
+#define lll_futex_timed_wait(futex, val, rt, private)     \
+        syscall(202, futex, FUTEX_WAIT_PRIVATE, val, rt, NULL, 0)
 
 #define lll_futex_wake(futex, nr, private)                             \
         syscall(202, futex, FUTEX_WAKE_PRIVATE, nr, NULL, NULL, 0)
@@ -154,10 +168,26 @@ __lll_lock_wait (int *futex, int private)
     lll_futex_wait (futex, 2, private); /* Wait if *futex == 2.  */
 }
 
+static void *handle = NULL;
+// Real functions
+#ifdef MTX_UNLOCK
+static int (*pthread_mutex_unlock_real)(pthread_mutex_t *mutex) = NULL;
+#endif
+#ifdef MTX_LOCK
+static int (*pthread_mutex_lock_real)(pthread_mutex_t *mutex) = NULL;
+#endif
+static int (*pthread_mutex_trylock_real)(pthread_mutex_t *mutex) = NULL;
+static int (*pthread_rwlock_rdlock_real)(pthread_rwlock_t *rwlock) = NULL;
+static int (*pthread_rwlock_tryrdlock_real)(pthread_rwlock_t *rwlock) = NULL;
+static int (*pthread_rwlock_wrlock_real)(pthread_rwlock_t *rwlock) = NULL;
+static int (*pthread_rwlock_trywrlock_real)(pthread_rwlock_t *rwlock) = NULL;
+
+#ifdef SCHED_REP
 int pthread_cond_signal(pthread_cond_t *cond)
 {
     int pshared = LLL_PRIVATE;
     syscall(319);
+    PRINT("cond_sig %llx\n", &cond->__data.__lock);
     lll_lock(cond->__data.__lock, pshared);
 
     if (cond->__data.__total_seq > cond->__data.__wakeup_seq) {
@@ -172,8 +202,8 @@ int pthread_cond_signal(pthread_cond_t *cond)
         lll_futex_wake(&cond->__data.__futex, 1, pshared);
     }
 
-    lll_unlock(cond->__data.__lock, pshared);
     syscall(320);
+    lll_unlock(cond->__data.__lock, pshared);
 
     return 0;
 }
@@ -182,18 +212,39 @@ int pthread_cond_wait(pthread_cond_t *cond, pthread_mutex_t *mutex)
 {
     int err;
     int pshared = LLL_PRIVATE;
+
+    void *handle;
+    handle = dlopen(PTHREAD_PATH, RTLD_LAZY);
+    if (!handle) {
+        printf("cannot open!\n");
+    }
+    if (!pthread_mutex_lock_real)
+        pthread_mutex_lock_real = dlsym(handle, "pthread_mutex_lock");
+#ifdef MTX_UNLOCK
+    if (!pthread_mutex_unlock_real)
+        pthread_mutex_unlock_real = dlsym(handle, "pthread_mutex_unlock");
+#endif
+
     // We're gonna have our own compact implementation of cond_wait so that we won't
     // miss the lock acquisitions inside pthread_cond_wait.
     // Here we are gonna support only basic cond_wait. PI futex, pthread_cancel, shared
     // futex are not supportted
 
     syscall(319);
+    PRINT("[%d] cond_wait %llx\n", syscall(186), &cond->__data.__lock);
     lll_lock(cond->__data.__lock, pshared);
     syscall(320);
     // pthread_mutex_unlock will do --mutex->__data.__nusers, however for the unlock
     // in cond_wait should not do this, so we add it by one first.
     ++mutex->__data.__nusers;
+    syscall(319);
+    PRINT("[%d] cond_wait mtx %llx\n", syscall(186), mutex);
+#ifdef MTX_UNLOCK
+    err = pthread_mutex_unlock_real(mutex);
+#else
     err = pthread_mutex_unlock(mutex);
+#endif
+    syscall(320);
     if (__builtin_expect(err, 0)) {
         lll_unlock(cond->__data.__lock, pshared);
         return err;
@@ -235,43 +286,203 @@ bc_out:
     lll_unlock(cond->__data.__lock, pshared);
 
     syscall(319);
-    err = pthread_mutex_lock(mutex);
+    err = pthread_mutex_lock_real(mutex);
     syscall(320);
     return err;
 }
 
-/*
+int pthread_cond_timedwait (cond, mutex, abstime)
+     pthread_cond_t *cond;
+     pthread_mutex_t *mutex;
+     const struct timespec *abstime;
+{
+  int result = 0;
+  int pshared = LLL_PRIVATE;
+
+  /* Make sure we are alone.  */
+  syscall(319);
+  PRINT("[%d] timed_cond_wait %llx\n", syscall(186), &cond->__data.__lock);
+  lll_lock (cond->__data.__lock, pshared);
+  syscall(320);
+
+  /* Now we can release the mutex.  */
+   ++mutex->__data.__nusers;
+   syscall(319);
+   PRINT("[%d] timed_cond_wait mtx %llx\n", syscall(186), &cond->__data.__lock);
+#ifdef MTX_UNLOCK
+   int err = pthread_mutex_unlock_real(mutex);
+#else
+   int err = pthread_mutex_unlock(mutex);
+#endif
+   syscall(320);
+  if (err)
+    {
+      lll_unlock (cond->__data.__lock, pshared);
+      return err;
+    }
+
+  /* We have one new user of the condvar.  */
+  syscall(319);
+  ++cond->__data.__total_seq;
+  ++cond->__data.__futex;
+  syscall(320);
+  cond->__data.__nwaiters += 1 << COND_NWAITERS_SHIFT;
+
+  /* Work around the fact that the kernel rejects negative timeout values
+     despite them being valid.  */
+  if (__builtin_expect (abstime->tv_sec < 0, 0))
+    goto timeout;
+
+  /* Remember the mutex we are using here.  If there is already a
+     different address store this is a bad user bug.  Do not store
+     anything for pshared condvars.  */
+  if (cond->__data.__mutex != (void *) ~0l)
+    cond->__data.__mutex = mutex;
+
+  /* The current values of the wakeup counter.  The "woken" counter
+     must exceed this value.  */
+  unsigned long long int val;
+  unsigned long long int seq;
+  unsigned long long int bc_seq;
+  val = seq = cond->__data.__wakeup_seq;
+  /* Remember the broadcast counter.  */
+  bc_seq = cond->__data.__broadcast_seq;
+
+  while (1)
+    {
+      struct timespec rt;
+      {
+	/* Get the current time.  So far we support only one clock.  */
+	struct timeval tv;
+	(void) gettimeofday (&tv, NULL);
+
+	/* Convert the absolute timeout value to a relative timeout.  */
+	rt.tv_sec = abstime->tv_sec - tv.tv_sec;
+	rt.tv_nsec = abstime->tv_nsec - tv.tv_usec * 1000;
+      }
+      if (rt.tv_nsec < 0)
+	{
+	  rt.tv_nsec += 1000000000;
+	  --rt.tv_sec;
+	}
+      /* Did we already time out?  */
+      if (__builtin_expect (rt.tv_sec < 0, 0))
+	{
+	  if (bc_seq != cond->__data.__broadcast_seq)
+	    goto bc_out;
+
+	  goto timeout;
+	}
+
+      unsigned int futex_val = cond->__data.__futex;
+
+      /* Prepare to wait.  Release the condvar futex.  */
+      lll_unlock (cond->__data.__lock, pshared);
+
+    {
+      /* Wait until woken by signal or broadcast.  */
+      syscall(321, 0);
+      err = lll_futex_timed_wait (&cond->__data.__futex,
+          futex_val, &rt, pshared);
+      PRINT("[%d] fxtret %d %llx\n", syscall(186), err, &cond->__data.__lock);
+    }
+
+      /* We are going to look at shared data again, so get the lock.  */
+      syscall(319);
+      lll_lock (cond->__data.__lock, pshared);
+      syscall(320);
+
+      /* If a broadcast happened, we are done.  */
+      if (bc_seq != cond->__data.__broadcast_seq)
+	goto bc_out;
+
+      /* Check whether we are eligible for wakeup.  */
+      val = cond->__data.__wakeup_seq;
+      if (val != seq && cond->__data.__woken_seq != val)
+	break;
+
+      /* Not woken yet.  Maybe the time expired?  */
+      if (__builtin_expect (err == -ETIMEDOUT, 0))
+	{
+	timeout:
+	  /* Yep.  Adjust the counters.  */
+    syscall(319);
+	  ++cond->__data.__wakeup_seq;
+	  ++cond->__data.__futex;
+    syscall(320);
+
+	  /* The error value.  */
+	  result = ETIMEDOUT;
+	  break;
+	}
+    }
+
+  /* Another thread woken up.  */
+  ++cond->__data.__woken_seq;
+
+ bc_out:
+  cond->__data.__nwaiters -= 1 << COND_NWAITERS_SHIFT;
+
+  lll_unlock (cond->__data.__lock, pshared);
+
+  syscall(319);
+  err = pthread_mutex_lock_real(mutex);
+  syscall(320);
+
+  int ret = err ? err : result;
+  PRINT("[%d] fxt err %d\n", ret);
+  return ret;
+}
+#endif
+
+#ifdef MTX_UNLOCK
+int pthread_mutex_unlock(pthread_mutex_t *mutex)
+{
+    int ret;
+    if (!handle) {
+        handle = dlopen(PTHREAD_PATH, RTLD_LAZY);
+    }
+    if (!pthread_mutex_unlock_real)
+        pthread_mutex_unlock_real = dlsym(handle, "pthread_mutex_unlock");
+
+    syscall(319);
+    ret = pthread_mutex_unlock_real(mutex);
+    syscall(320);
+
+    return ret;
+}
+#endif
+
+#ifdef MTX_LOCK
 int pthread_mutex_lock(pthread_mutex_t *mutex)
 {
     int ret;
-    void *handle;
-    static int (*pthread_mutex_lock_real)(pthread_mutex_t *mutex) = NULL;
-    handle = dlopen(PTHREAD_PATH, RTLD_LAZY);
     if (!handle) {
-        printf("cannot open!\n");
+        handle = dlopen(PTHREAD_PATH, RTLD_LAZY);
     }
     if (!pthread_mutex_lock_real)
         pthread_mutex_lock_real = dlsym(handle, "pthread_mutex_lock");
 
     syscall(319);
+    PRINT("mtx %llx\n", mutex);
     ret = pthread_mutex_lock_real(mutex);
     syscall(320);
 
     return ret;
 }
-
+#endif
 
 int pthread_mutex_trylock(pthread_mutex_t *mutex)
 {
     int ret;
-    void *handle;
-    static int (*pthread_mutex_trylock_real)(pthread_mutex_t *mutex) = NULL;
-    int (*pthread_mutex_lock_real)(pthread_mutex_t *mutex) = NULL;
-    handle = dlopen(PTHREAD_PATH, RTLD_LAZY);
+    if (!handle) {
+        handle = dlopen(PTHREAD_PATH, RTLD_LAZY);
+    }
     if (!pthread_mutex_trylock_real)
         pthread_mutex_trylock_real = dlsym(handle, "pthread_mutex_trylock");
 
     syscall(319);
+    PRINT("try mtx %llx\n", mutex);
     ret = pthread_mutex_trylock_real(mutex);
     syscall(320);
 
@@ -282,8 +493,6 @@ int pthread_rwlock_rdlock(pthread_rwlock_t *rwlock)
 {
     int ret;
     void *handle;
-    static int (*pthread_rwlock_rdlock_real)(pthread_rwlock_t *rwlock) = NULL;
-    int (*pthread_mutex_lock_real)(pthread_mutex_t *mutex) = NULL;
     handle = dlopen(PTHREAD_PATH, RTLD_LAZY);
     if (!pthread_rwlock_rdlock_real)
         pthread_rwlock_rdlock_real = dlsym(handle, "pthread_rwlock_rdlock");
@@ -298,10 +507,9 @@ int pthread_rwlock_rdlock(pthread_rwlock_t *rwlock)
 int pthread_rwlock_tryrdlock(pthread_rwlock_t *rwlock)
 {
     int ret;
-    void *handle;
-    static int (*pthread_rwlock_tryrdlock_real)(pthread_rwlock_t *rwlock) = NULL;
-    int (*pthread_mutex_lock_real)(pthread_mutex_t *mutex) = NULL;
-    handle = dlopen(PTHREAD_PATH, RTLD_LAZY);
+    if (!handle) {
+        handle = dlopen(PTHREAD_PATH, RTLD_LAZY);
+    }
     if (!pthread_rwlock_tryrdlock_real)
         pthread_rwlock_tryrdlock_real = dlsym(handle, "pthread_rwlock_tryrdlock");
 
@@ -315,8 +523,9 @@ int pthread_rwlock_tryrdlock(pthread_rwlock_t *rwlock)
 int pthread_rwlock_wrlock(pthread_rwlock_t *rwlock)
 {
     int ret;
-    void *handle;
-    static int (*pthread_rwlock_wrlock_real)(pthread_rwlock_t *rwlock) = NULL;
+    if (!handle) {
+        handle = dlopen(PTHREAD_PATH, RTLD_LAZY);
+    }
     if (!pthread_rwlock_wrlock_real)
         pthread_rwlock_wrlock_real = dlsym(handle, "pthread_rwlock_wrlock");
 
@@ -330,10 +539,9 @@ int pthread_rwlock_wrlock(pthread_rwlock_t *rwlock)
 int pthread_rwlock_trywrlock(pthread_rwlock_t *rwlock)
 {
     int ret;
-    void *handle;
-    static int (*pthread_rwlock_trywrlock_real)(pthread_rwlock_t *rwlock) = NULL;
-    int (*pthread_mutex_lock_real)(pthread_mutex_t *mutex) = NULL;
-    handle = dlopen(PTHREAD_PATH, RTLD_LAZY);
+    if (!handle) {
+        handle = dlopen(PTHREAD_PATH, RTLD_LAZY);
+    }
     if (!pthread_rwlock_trywrlock_real)
         pthread_rwlock_trywrlock_real = dlsym(handle, "pthread_rwlock_trywrlock");
 
@@ -343,4 +551,3 @@ int pthread_rwlock_trywrlock(pthread_rwlock_t *rwlock)
 
     return ret;
 }
-*/
