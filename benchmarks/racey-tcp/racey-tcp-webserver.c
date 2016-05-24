773e86c1058cb1005d02edaf52c0ae046f532234
root <root@Jupiter.(none)>
2016-05-24 11:49:31 -0400

    latest bench
    Former-commit-id: 1697a48cce77e87d373c6c4b96b32844f85fb2d0

--- benchmarks/racey-tcp/racey-tcp-webserver.c
+++ benchmarks/racey-tcp/racey-tcp-webserver.c
@@ -15,18 +15,28 @@
 #include <fcntl.h>
 #include <pthread.h>
 #include <sys/syscall.h>
+#include <errno.h>
 
-#define WORKER_NUM 5
+#define LINUX
+
+#ifndef LINUX
+#define POPCORN_SYSCALL_1(x) syscall(x);
+#define POPCORN_SYSCALL_2(x,y) syscall(x,y);
+#else
+#define POPCORN_SYSCALL_1(x) ;
+#define POPCORN_SYSCALL_2(x,y) ;
+#endif
+
 #define OUTPUT_FILE "./output.txt"
-#define MAX_BUFF (500)
+#define MAX_BUFF (1000)
 
 pthread_cond_t cv = PTHREAD_COND_INITIALIZER;
 
-int client_fds[WORKER_NUM];
+int *client_fds;
 int client_idx;
 int output_fd;
 int barrier;
-int connection_before_error;
+int worker_threads;
 int fault_count = 0;
 int have_fd = 0;
 pthread_mutex_t client_fds_lock;
@@ -83,15 +93,15 @@ int get_file_size(int fd) {
 int send_new(int fd, char *msg, int len) {
 	//int len = strlen(msg);
 	int ret;
-	
+
 	if(len==0){
 		len= strlen(msg);
 	}
-	syscall(319);
+	POPCORN_SYSCALL_1(319);
 	ret= send(fd, msg, len, 0);
-	syscall(320);
+	POPCORN_SYSCALL_1(320);
 	if (ret == -1) {
-		printf("Error in send\n");
+		printf("Error in send %s\n",strerror(errno));
 	}
 
 	return ret;
@@ -101,19 +111,19 @@ int send_new(int fd, char *msg, int len) {
    Handles php requests
  */
 void php_cgi(char* script_path, int fd) {
-        send_new(fd, "HTTP/1.1 200 OK\n Server: Web Server in C\n Connection: close\n", 0);
-        dup2(fd, STDOUT_FILENO);
-        char script[500];
-        strcpy(script, "SCRIPT_FILENAME=");
-        strcat(script, script_path);
-        putenv("GATEWAY_INTERFACE=CGI/1.1");
-        putenv(script);
-        putenv("QUERY_STRING=");
-        putenv("REQUEST_METHOD=GET");
-        putenv("REDIRECT_STATUS=true");
-        putenv("SERVER_PROTOCOL=HTTP/1.1");
-        putenv("REMOTE_HOST=127.0.0.1");
-        execl("/usr/bin/php-cgi", "php-cgi", NULL);
+	send_new(fd, "HTTP/1.1 200 OK\n Server: Web Server in C\n Connection: close\n", 0);
+	dup2(fd, STDOUT_FILENO);
+	char script[500];
+	strcpy(script, "SCRIPT_FILENAME=");
+	strcat(script, script_path);
+	putenv("GATEWAY_INTERFACE=CGI/1.1");
+	putenv(script);
+	putenv("QUERY_STRING=");
+	putenv("REQUEST_METHOD=GET");
+	putenv("REDIRECT_STATUS=true");
+	putenv("SERVER_PROTOCOL=HTTP/1.1");
+	putenv("REMOTE_HOST=127.0.0.1");
+	execl("/usr/bin/php-cgi", "php-cgi", NULL);
 }
 
 /*
@@ -142,9 +152,9 @@ int recv_new(int fd, char *buffer) {
 	char *p = buffer; // Use of a pointer to the buffer rather than dealing with the buffer directly
 	int eol_matched = 0; // Use to check whether the recieved byte is matched with the buffer byte or not
 	int ret;
-	syscall(319);	
+	POPCORN_SYSCALL_1(319);	
 	ret= recv(fd, p, 1, 0);
-	syscall(320);
+	POPCORN_SYSCALL_1(320);
 	while (ret != 0) // Start receiving 1 byte at a time
 	{
 		if (*p == EOL[eol_matched]) // if the byte matches with the first eol byte that is '\r'
@@ -159,13 +169,42 @@ int recv_new(int fd, char *buffer) {
 			eol_matched = 0;
 		}
 		p++; // Increment the pointer to receive next byte
-		syscall(319);
+		POPCORN_SYSCALL_1(319);
 		ret= recv(fd, p, 1, 0);
-		syscall(320);
+		POPCORN_SYSCALL_1(320);
 	}
 	return (0);
 }
 
+static int set_non_blocking_mode(int sock) {
+  unsigned long on = 1;
+  return ioctlsocket(sock, FIONBIO, &on);
+}
+
+static void close_socket_gracefully(int sock) {
+  struct linger linger;
+
+  // Set linger option to avoid socket hanging out after close. This prevent
+  // ephemeral port exhaust problem under high QPS.
+  linger.l_onoff = 1;
+  linger.l_linger = 1;
+  
+  POPCORN_SYSCALL_1(319);
+  setsockopt(sock, SOL_SOCKET, SO_LINGER, (char *) &linger, sizeof(linger));        
+  POPCORN_SYSCALL_1(320);
+
+  POPCORN_SYSCALL_1(319);
+  // Send FIN to the client
+  shutdown(sock, SHUT_WR);
+  set_non_blocking_mode(sock);
+  POPCORN_SYSCALL_1(320);
+  // Now we know that our FIN is ACK-ed, safe to close
+  POPCORN_SYSCALL_1(319);
+  close(sock);
+  POPCORN_SYSCALL_1(320);
+}
+
+volatile unsigned long long busy_loop=0;
 /*
    This function parses the HTTP requests,
    arrange resource locations,
@@ -173,143 +212,145 @@ int recv_new(int fd, char *buffer) {
    serves files in a web root,
    sends the HTTP error codes.
  */
-int connection(int fd, char* buff) {
-	char request[500], resource[500], *ptr;
+int connection(int fd, char *request, char *resource, char* buff) {
+	//char request[500], resource[500], *ptr;
+	char *ptr;
 	int fd1,length;
 
 	if (recv_new(fd, request) == 0) {
 		printf("Recieve Failed\n");
 	}
 
-	printf("%s\n", request);
+//	printf("%s\n", request);
 
 	// Check for a valid browser request
 	ptr = strstr(request, " HTTP/");
 	if (ptr == NULL) {
 		printf("NOT HTTP !\n\n");
-	} else {
-		*ptr = 0;
-		ptr = NULL;
+		goto out;
+	} 
+	*ptr = 0;
+	ptr = NULL;
 
-		if (strncmp(request, "GET ", 4) == 0) {
-			ptr = request + 4;
-		}
-		if (ptr == NULL) {
-			printf("Unknown Request ! \n\n");
-		} else {
-			if (ptr[strlen(ptr) - 1] == '/') {
-				strcat(ptr, "index.html");
-			}
-			//strcpy(resource, webroot());
-			strcpy(resource, "/root/web/");
-			strcat(resource, ptr);
-			char* s = strchr(ptr, '.');
-			int i;
-			for (i = 0; extensions[i].ext != NULL; i++) {
-				if (strcmp(s + 1, extensions[i].ext) == 0) {
-
-					fd1 = open(resource, O_RDONLY, 0);
-					//printf("Opening \"%s\"\n", resource);
-					if (fd1 == -1) {
-						printf("404 File not found Error\n\n");
-						send_new(fd, "HTTP/1.1 404 Not Found\r\n", 0);
-						send_new(fd, "Server : Web Server in C\r\n\r\n", 0);
-						send_new(fd, "<html><head><title>404 Not Found</head></title>", 0);
-						send_new(fd, "<body><p>404 Not Found: The requested resource could not be found!</p></body></html>", 0);
-						goto out;
-						//Handling php requests
-					} else if (strcmp(extensions[i].ext, "php") == 0) {
-						printf("Seeee php. Don't try...\n");
-						//php_cgi(resource, fd);
-						//sleep(1);
-
-					} else {
-						//printf("200 OK, Content-Type: %s\n\n",extensions[i].mediatype);
-						send_new(fd, "HTTP/1.1 200 OK\r\n", 0);
-						send_new(fd, "Server : Web Server in C\r\n\r\n", 0);
-						if (ptr == request + 4) // if it is a GET request
-						{
-							if ((length = get_file_size(fd1)) == -1)
-								printf("Error in getting size !\n");
-							int total_bytes_sent = 0;
-							int bytes_sent;
-							int bytes_read;
-							while (total_bytes_sent < length) {
-								
-								syscall(319);
-								bytes_read= read(fd1, buff, MAX_BUFF);
-								syscall(320);
-								if(bytes_read > 0){
-									//if ((bytes_sent = sendfile(fd, fd1, 0,
-									//				length - total_bytes_sent)) <= 0) {
-									
-									if((bytes_sent = send_new(fd, buff, bytes_read)) <= 0){
-										printf("send file error\n");
-										break;	
-									}
-									total_bytes_sent += bytes_sent;
-								}
-								else{
-									break;
-								}
-							}
-
-						}
-					}	
-					syscall(319);
-					close(fd1);
-					syscall(320);
-					goto out;
-				}
-				int size = sizeof(extensions) / sizeof(extensions[0]);
-				if (i == size - 2) {
-					printf("415 Unsupported Media Type\n\n");
-					send_new(fd, "HTTP/1.1 415 Unsupported Media Type\r\n", 0);
-					send_new(fd, "Server : Web Server in C\r\n\r\n", 0);
-					send_new(fd, "<html><head><title>415 Unsupported Media Type</head></title>", 0);
-					send_new(fd, "<body><p>415 Unsupported Media Type!</p></body></html>", 0);
-				}
-			}
-
-			//close(fd1);
-		}
+	if (strncmp(request, "GET ", 4) == 0) {
+		ptr = request + 4;
 	}
+	if (ptr == NULL) {
+		printf("Unknown Request ! \n\n");
+		goto out;
+	} 
+	if (ptr[strlen(ptr) - 1] == '/') {
+		strcat(ptr, "index.html");
+	}
+	//strcpy(resource, webroot());
+	//printf("before\n");
+	strcpy(resource, "/root/web/");
+	strcat(resource, ptr);
+	//printf("resource %s\n", resource);
 
+	fd1 = open(resource, O_RDONLY, 0);
+	//printf("Opening \"%s\"\n", resource);
+	if (fd1 == -1) {
+		printf("404 File not found Error\n\n");
+		send_new(fd, "HTTP/1.1 404 Not Found\r\n", 0);
+		send_new(fd, "Server : Web Server in C\r\n\r\n", 0);
+		send_new(fd, "<html><head><title>404 Not Found</head></title>", 0);
+		send_new(fd, "<body><p>404 Not Found: The requested resource could not be found!</p></body></html>", 0);
+		goto out;
+	} 
+	//printf("200 OK\n\n");
+	send_new(fd, "HTTP/1.1 200 OK\r\n", 0);
+	if ((length = get_file_size(fd1)) == -1)
+		printf("Error in getting size !\n");
+	send_new(fd, "Content-Type: text/plain\r\n", 0);
+	sprintf(resource, "Content-Length: %d\r\n", length);
+	send_new(fd, resource, 0);
+	//send_new(fd, "Connection: close\r\n", 0);
+	send_new(fd, "Accept-Ranges: bytes\r\n", 0);
+	send_new(fd, "Server : Web Server in C\r\n\r\n", 0);
+	if (ptr == request + 4) // if it is a GET request
+	{
+
+		volatile unsigned long long bla=0;
+		while(bla< (1<<18) ){
+			bla++;
+			busy_loop++;
+		}
+
+		int total_bytes_sent = 0;
+		int bytes_sent;
+		int bytes_read;
+		while (total_bytes_sent < length) {
+
+			POPCORN_SYSCALL_1(319);
+			bytes_read= read(fd1, buff, MAX_BUFF);
+			POPCORN_SYSCALL_1(320);
+			if(bytes_read > 0){
+			//printf("sending %d bytes\n",bytes_read);
+				if((bytes_sent = send_new(fd, buff, bytes_read)) <= 0){
+					POPCORN_SYSCALL_1(319);
+                			close(fd1);
+                			POPCORN_SYSCALL_1(320);
+					goto out;	
+				}
+				total_bytes_sent += bytes_sent;
+			//printf("sent %d bytes\n",bytes_sent);
+			}
+			else{
+				break;
+			}
+
+		}
+		printf("sent %d bytes\n", total_bytes_sent);
+		recv_new(fd, buff);
+		POPCORN_SYSCALL_1(319);
+		close(fd1);
+		POPCORN_SYSCALL_1(320);
+
+	}
 out:
-	syscall(319);
-	shutdown(fd, SHUT_RDWR);
-	close(fd);
-	syscall(320);
+	close_socket_gracefully(fd);
 }
 
 void* racey_worker(void* data)
 {
 	int fd;
 	int n = 0;
-	char buf[256];
+	//char buf[256];
 	int tid = *(int *)data;
 	struct sockaddr_in myAddr;
-        socklen_t mylen;
+	socklen_t mylen;
 
-	/*char *buff= malloc(MAX_BUFF);
+	char *buff= malloc(MAX_BUFF);
 	if(!buff){
 		printf("Impossible to malloc\n");
 		exit(1);
-	}*/
-	char buff[500];
+	}
+
+	char *req= malloc(MAX_BUFF*2);
+	if(!req){
+		printf("Impossible to malloc\n");
+		exit(1);
+	}
+
+	char* res= malloc(MAX_BUFF*2);
+	if(!res){
+		printf("Impossible to malloc\n");
+		exit(1);
+	}
 
 	while (barrier == 0) {}
 
 	for (;;) {
 		/* Waiting for an availiable socket */
 		for (;;) {
-			syscall(319);
+			POPCORN_SYSCALL_1(319);
 			pthread_mutex_lock(&client_fds_lock);
-			syscall(320);
+			POPCORN_SYSCALL_1(320);
 			while (have_fd == 0) {
-				syscall(319);
+				POPCORN_SYSCALL_1(319);
 				pthread_cond_wait(&cv, &client_fds_lock);
-				syscall(320);
+				POPCORN_SYSCALL_1(320);
 			}
 			if ( client_idx>=0 && client_fds[client_idx] > 0) {
 				fd = client_fds[client_idx];
@@ -320,34 +361,18 @@ void* racey_worker(void* data)
 					have_fd = 0;
 				}
 				pthread_mutex_unlock(&client_fds_lock);
-				syscall(321, 1);
+				POPCORN_SYSCALL_2(321, 1);
 				break;
 			}
 			pthread_mutex_unlock(&client_fds_lock);
 		}
 
-	/*	if(fault_count==connection_before_error)
-			syscall(318);
-
-		do {
-			syscall(319);
-			n = read(fd, buf, sizeof(buf));
-			//n = recv(fd, buf + 10, sizeof(buf), 0);
-			syscall(320);
-			sprintf(buf, "\n%d, %d", tid, fd);
-			syscall(319);
-			write(output_fd, buf, n);
-			syscall(320);
-		} while (n > 0);
-
-		syscall(319);
-		close(fd);
-		syscall(320);*/
-		//mylen= sizeof(struct sockaddr_in);
-		//getpeername(fd, (struct sockaddr *) &myAddr, &mylen);
-		//printf("thread %d handling connection port %d\n", tid, ntohs(myAddr.sin_port));
-		connection(fd, &buff);
+		connection(fd, req, res, buff);
 	}
+
+	kfree(req);
+	kfree(res);
+	kfree(buff);
 }
 
 int main(int argc, char **argv)
@@ -359,20 +384,33 @@ int main(int argc, char **argv)
 	int server_fd;
 	int client_fd;
 	int i;
-	int tinfo[WORKER_NUM];
+	int *tinfo;
 	int accepted= 0;
-	pthread_t threads[WORKER_NUM];
+	pthread_t *threads;
 	pthread_attr_t attr;
 
 	if (argc != 3)     /* Test for correct number of arguments */
 	{
-		fprintf(stderr, "Usage:  %s <Server Port> <Connection_before_error>\n", argv[0]);
+		fprintf(stderr, "Usage:  %s <Server Port> <threads>\n", argv[0]);
 		exit(1);
 	}
 
 	server_port = atoi(argv[1]);  /* First arg:  local port */
 
-	connection_before_error=  atoi(argv[2]);
+	worker_threads=  atoi(argv[2]);
+
+	client_fds= malloc(sizeof(int)*worker_threads);
+	if(!client_fds){
+		return;
+	}
+	threads= malloc(sizeof(pthread_t)*worker_threads);
+	if(!threads){
+		return;
+	}
+	tinfo= malloc(sizeof(int)*worker_threads);
+	if(!tinfo){
+		return;
+	}
 
 	pthread_mutex_init(&client_fds_lock, NULL);
 	pthread_mutex_init(&file_lock, NULL);
@@ -402,7 +440,7 @@ int main(int argc, char **argv)
 
 	client_idx = -1;
 	barrier = 0;
-	for (i = 0; i < WORKER_NUM; i++) {
+	for (i = 0; i < worker_threads; i++) {
 		client_fds[i] = -1;
 		tinfo[i] = i;
 		if (pthread_create(&threads[i], &attr, racey_worker, &tinfo[i]) < 0) {
@@ -411,39 +449,39 @@ int main(int argc, char **argv)
 	}
 	barrier = 1;
 
-	if (listen(server_fd, 10) < 0) {
+	if (listen(server_fd, 20000) < 0) {
 		return 1;
 	}
 
 	for (;;) {
 		/* Accept whatever is coming to me */
 		client_len = sizeof(client_addr);
-		syscall(321, 20);
-		syscall(319);
+		POPCORN_SYSCALL_2(321, 20);
+		POPCORN_SYSCALL_1(319);
 		if ((client_fd = accept(server_fd, (struct sockaddr *) &client_addr,
 						&client_len)) < 0) {
-			syscall(320);
+			POPCORN_SYSCALL_1(320);
 			return 1;
 		}
-		accepted++;
-		if(accepted==connection_before_error)
-			syscall(318);
-
-		syscall(321, 10);
-		syscall(320);
+		/*accepted++;
+		  if(accepted==connection_before_error)
+		  POPCORN_SYSCALL_1(318);
+		 */
+		POPCORN_SYSCALL_2(321, 10);
+		POPCORN_SYSCALL_1(320);
 		while (1) {
-			syscall(319);
+			POPCORN_SYSCALL_1(319);
 			pthread_mutex_lock(&client_fds_lock);
-			syscall(320);
-			syscall(321, 3);
-			if (client_idx < WORKER_NUM -1) {
+			POPCORN_SYSCALL_1(320);
+			POPCORN_SYSCALL_2(321, 3);
+			if (client_idx < worker_threads-1) {
 				/* Feed the socket to workers */
 				client_idx ++;
 				have_fd = 1;
 				client_fds[client_idx] = client_fd;
-				syscall(319);
+				POPCORN_SYSCALL_1(319);
 				pthread_cond_signal(&cv);
-				syscall(320);
+				POPCORN_SYSCALL_1(320);
 				pthread_mutex_unlock(&client_fds_lock);
 				break;
 			}
